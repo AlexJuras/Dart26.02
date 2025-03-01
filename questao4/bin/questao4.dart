4fb9728a39c25fef88fc3d0e9d277307d00efb14
Aenn <annekarol.oliveirabatista@gmail.com>
2025-02-28 21:17:39 -0300

    erro corrigido

--- questao4/bin/questao4.dart
+++ questao4/bin/questao4.dart
@@ -4,28 +4,38 @@ void main()
 {
   List <String> opcoesBanco = <String> ['sacar', 'depositar','pagamento'];
   
-  void pagamento(){
+  String? pagamento(){
     stdout.write("Digite uma operação:(Sacar, Depositar ou Pagamento): ");
     String? opcao = stdin.readLineSync();
 
-    if(opcoesBanco.contains(opcao)){
-      stdout.write("Escreva o valor da operação: ");
-      double quantia = double.parse(stdin.readLineSync()!);
-      if (quantia > 0){
-        stdout.write("Operação escolhida: $opcao, Valor: R\$${quantia.toStringAsFixed(2)}");
-      }else{
-        stdout.write('Valor não válido, tente novamente');
-        pagamento();
-      }
-    }else{
+    if(!opcoesBanco.contains(opcao)){
       stdout.write("Operação inválida, tente novamente");
-      pagamento();
+      return pagamento();
     }
-    }
+      return opcao;
+  }
+  
+  double valores(){
+    stdout.write("Escreva o valor da operação: ");
+    double quantia = double.parse(stdin.readLineSync()!);
+  
+
+      if (quantia <= 0){
+        stdout.write('Valor não válido, tente novamente');
+        return valores();
+      }  
+      return quantia;
+    
+  }
+
+  var pag = pagamento();
+  var val = valores();
+
+  stdout.write("Operação escolhida: $pag, Valor: R\$${val.toStringAsFixed(2)}");
+  
 
 // print("Operação escolhida: $pagamento,$valor ");
-pagamento();
-  }
+}
    
 
 
